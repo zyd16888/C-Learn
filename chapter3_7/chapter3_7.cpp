e4a8ef3c759a7edd1af92b6831e252c1e0ac4f9d
zyd16888 <zyd475182659@163.com>
2019-04-01 22:20:38 +0800

    chapter3_7

--- chapter3_7/chapter3_7.cpp
+++ chapter3_7/chapter3_7.cpp
@@ -3,19 +3,23 @@
 #include "pch.h"
 #include <iostream>
 using namespace std;
-class Student
-{
+const class Student {
 public:
-	Student(int n, float s) :num(n), score(s) {  }
-	void change(int n, float s) { num = n; score = s; }
-	void display() { cout << num << " " << score << endl; }
+	const Student(int n, float s) :num(n), score(s) {}
+	const void change(int n, float s) {
+		num = n;
+		score = s;
+	}
+	const void display() {
+		cout << num << " " << score << endl;
+	}
 private:
 	int num;
 	float score;
 };
-int main()
-{
-	Student stud(101, 78.5);
+
+int main() {
+	const Student stud(101, 78.5);
 	stud.display();
 	stud.change(101, 80.5);
 	stud.display();
